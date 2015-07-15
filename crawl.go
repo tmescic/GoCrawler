e8baad5c92ac4213a66f56d9d4abdae6106152ad
tmescic <tihomir.mescic@gmail.com>
2015-07-15 14:47:57 +0200

    Working version

--- crawl.go
+++ crawl.go
@@ -22,27 +22,49 @@ func main() {
 	check(err)
 	defer file.Close()
 
-	// visited nodes are stored in this maps
-	visited := make(map[string]bool)
-
 	// queue of pages to be visited
 	queue := make([]string, 0)
 
+	// put all seeds to queue
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		currentUrl := scanner.Text()
-		fmt.Println("Visiting   :", currentUrl)
-		resp, err2 := http.Get(currentUrl)
-		check(err2)
-
-		fmt.Println("StatusCode :", resp.StatusCode)
-
-		parsePage(resp.Body, queue)
-		visited[currentUrl] = true;
+		url := scanner.Text()
+		queue = append(queue, url)
 	}
-
 	err3 := scanner.Err()
 	check(err3)
+
+
+	// visited nodes are stored in this maps
+	visited := make(map[string]bool)
+
+	// visit pages from the queue
+	for len(queue) > 0 {
+		url := queue[0];
+		queue = queue[1:] 
+		oldQueueLen := len(queue)
+		fmt.Print("Fetching :", url)
+		resp, err2 := http.Get(url)
+		check(err2)
+
+		fmt.Print(", response: ", resp.StatusCode)
+		fmt.Print(", parsing... ")
+		
+		links := parsePage(resp.Body)
+
+		visited[url] = true;
+		
+		fmt.Print(" links total: ", len(links))
+
+		// see if they are already visited
+		for _,link := range links {
+			if !visited[link] {
+				queue = append(queue, link)
+			}
+		}
+
+		fmt.Println(", new links: ", (len(queue) - oldQueueLen) )
+	}
 
 	fmt.Println("Not a crawler yet, but I'm getting there...")
 	fmt.Println("Queue size: ", len(queue))
@@ -50,28 +72,30 @@ func main() {
 	
 }
 
-func parsePage(body io.ReadCloser, queue []string) {
+func parsePage(body io.ReadCloser) []string {
 
 	doc, err := html.Parse(body)
 	if err != nil {
 		fmt.Println("Error : ", err)
 	}
 
-	var f func(*html.Node)
-	f = func(n *html.Node) {
+	var visit func(*html.Node) []string
+	visit = func(n *html.Node) []string {
+		links := make([]string, 0)
+
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for i := 0; i < len(n.Attr); i++ {
 				if n.Attr[i].Key == "href" {
-					fmt.Println("new link: ", n.Attr[i].Val)
-					queue = append(queue, n.Attr[i].Val)
+					// fmt.Println("new link: ", n.Attr[i].Val)
+					links = append(links, n.Attr[i].Val)
 				}
 			}
 		}
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			f(c)
+			links = append(links, visit(c)...)
 		}
+		return links
 	}
-	f(doc)
-
-	return
+	
+	return visit(doc)
 }
